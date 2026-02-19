a09eb9a4c35dbeeb70a2451d78f4a14c78db202b
xnmp <chonw89@gmail.com>
2026-02-20 10:38:48 +1100

    deleted unused scratchpad files

--- dot_local/bin/executable_lite-xl-scratchpad.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Toggle lite-xl scratchpad. Launches into special workspace if not running.
-
-if ! pgrep -x lite-xl > /dev/null 2>&1; then
-    hyprctl dispatch exec "[workspace special:lite-xl;float;size 1400 900;center] lite-xl"
-else
-    hyprctl dispatch togglespecialworkspace lite-xl
-fi
