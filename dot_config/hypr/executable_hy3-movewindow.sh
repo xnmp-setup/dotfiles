6762d15d0815390e94ad9a089a8ca4e9035cfd3b
xnmp <chonw89@gmail.com>
2026-02-20 22:37:29 +1100

    move across window makes more sense

--- dot_config/hypr/executable_hy3-movewindow.sh
+++ dot_config/hypr/executable_hy3-movewindow.sh
@@ -3,13 +3,13 @@
 dir="$1"
 
 # Capture window position before move
-before=$(hyprctl activewindow -j | jq '[.at[0], .at[1], .monitor]')
+before=$(hyprctl activewindow -j | jq '[.at, .size, .monitor]')
 
 # Try hy3 move
 hyprctl dispatch hy3:movewindow "$dir"
 
 # Capture after
-after=$(hyprctl activewindow -j | jq '[.at[0], .at[1], .monitor]')
+after=$(hyprctl activewindow -j | jq '[.at, .size, .monitor]')
 
 # If nothing changed, move across monitors
 if [[ "$before" == "$after" ]]; then
